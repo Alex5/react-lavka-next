144fc7aaa2e545f860e261f5a137bba7e919f2ab
alex5 <angrymarshall@gmail.com>
2025-06-24 16:18:17 +0300

    feat() code

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: "standalone",
     images: {
         remotePatterns: [new URL('https://avatars.mds.yandex.net/**'), new URL('https://yastatic.net/**')],
     },
