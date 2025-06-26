323f92514c2fc93eee059ffdc212aae50019f075
alex5 <angrymarshall@gmail.com>
2025-06-26 21:42:39 +0300

    feat() fix auth

--- lib/api/cart.ts
+++ lib/api/cart.ts
@@ -1,11 +1,8 @@
 import {fetcher} from "@/lib/api/fetcher";
 import {CartType} from "@/shared/api/hooks/use-cart/use-cart.types";
-import {headers as nextHeaders} from "next/dist/server/request/headers";
 
 export async function getCart(): Promise<{ cart: CartType | undefined }> {
-    const headers = await nextHeaders();
-
-    const res = await fetcher("cart", {headers});
+    const res = await fetcher("cart");
 
     if (res.status === 401) return {
         cart: undefined
--- lib/api/user.ts
+++ lib/api/user.ts
@@ -1,11 +1,9 @@
 import {YandexUserInfo} from "@/shared/api/hooks/use-auth/use-auth";
 import {fetcher} from "@/lib/api/fetcher";
-import {headers} from "next/headers";
 
 export async function getUser(): Promise<{ user: YandexUserInfo | null }> {
     const response = await fetcher(`me`, {
         credentials: "include",
-        headers: await headers()
     })
 
     if (response.status === 401) return {user: null};
