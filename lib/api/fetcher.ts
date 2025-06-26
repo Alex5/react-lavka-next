54cf9217b7b64659415885dfccbd2dd72bfbe510
alex5 <angrymarshall@gmail.com>
2025-06-26 21:32:29 +0300

    feat() fix auth

--- lib/api/fetcher.ts
+++ lib/api/fetcher.ts
@@ -1,8 +1,5 @@
 import {APP_CONFIG} from "@/lavka.config";
-import {headers as nextHeaders} from "next/dist/server/request/headers";
 
-export async function fetcher(input: string | URL | globalThis.Request,init?: RequestInit) {
-    const headers = await nextHeaders();
-
-    return fetch(`${APP_CONFIG.API_URL}/api/v1/${input}`, {...init, headers})
+export async function fetcher(input: string | URL | globalThis.Request, init?: RequestInit) {
+    return fetch(`${APP_CONFIG.API_URL}/api/v1/${input}`, init)
 }
--- lib/api/user.ts
+++ lib/api/user.ts
@@ -1,9 +1,11 @@
 import {YandexUserInfo} from "@/shared/api/hooks/use-auth/use-auth";
 import {fetcher} from "@/lib/api/fetcher";
+import {headers} from "next/headers";
 
 export async function getUser(): Promise<{ user: YandexUserInfo | null }> {
     const response = await fetcher(`me`, {
-        credentials: 'include'
+        credentials: "include",
+        headers: await headers()
     })
 
     if (response.status === 401) return {user: null};
